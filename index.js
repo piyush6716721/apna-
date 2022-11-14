7706af30e4d98abe8e23070318edf5fa6d845f10
piyush6716721 <113968283+piyush6716721@users.noreply.github.com>
2022-11-14 14:47:06 +0530

    hdhdhdfcfl

---
+++ index.js
@@ -0,0 +1,12 @@
+let EXPLOREbtn=document.querySelector('button');
+EXPLOREbtn.addEventListener('click' , inputmsg);
+
+function inputmsg(){
+    let name=prompt('enter name of student');
+    EXPLOREbtn.textcont='roll no.1 '+name;
+    let c=prompt('enter name for your course');
+    EXPLOREbtn.textcont='roll no.1 '+name;
+    let course=prompt('enter name of codding language want you learn');
+     EXPLOREbtn.textcont='roll no.1 '+name;
+}
+
